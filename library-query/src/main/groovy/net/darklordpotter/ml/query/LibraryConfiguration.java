bcff30cfd40c1bcb5733a6a334c024f07cc27b1f
Michael Rose <elementation@gmail.com>
2013-03-23 17:03:10 -0600

    Adding new config & healthcheck

---
+++ library-query/src/main/groovy/net/darklordpotter/ml/query/LibraryConfiguration.java
@@ -0,0 +1,14 @@
+package net.darklordpotter.ml.query;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.yammer.dropwizard.config.Configuration;
+
+/**
+ * 2013-03-23
+ *
+ * @author Michael Rose <michael@fullcontact.com>
+ */
+public class LibraryConfiguration extends Configuration {
+    @JsonProperty
+    public String mongoDsn = "mongodb://localhost/dlp-library";
+}
---
+++ library-query/src/main/groovy/net/darklordpotter/ml/query/healthcheck/MongoHealthCheck.java
@@ -0,0 +1,26 @@
+package net.darklordpotter.ml.query.healthcheck;
+
+import com.mongodb.MongoClient;
+import com.mongodb.ReplicaSetStatus;
+import com.yammer.metrics.core.HealthCheck;
+
+/**
+ * 2013-03-23
+ *
+ * @author Michael Rose <michael@fullcontact.com>
+ */
+public class MongoHealthCheck extends HealthCheck {
+    private final MongoClient client;
+
+
+    public MongoHealthCheck(MongoClient client) {
+        super("mongodb");
+
+        this.client = client;
+    }
+
+    @Override
+    protected Result check() throws Exception {
+        return Result.healthy(client.getConnector().debugString());
+    }
+}
