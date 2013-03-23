a907b96a5ea3fa2bab4c5cbe282c2bb0de7a85f6
Michael Rose <elementation@gmail.com>
2013-03-23 11:36:32 -0600

    Fixing CORS headers to include a max age

--- library-query/src/main/groovy/net/darklordpotter/ml/query/CORSFilter.groovy
+++ library-query/src/main/groovy/net/darklordpotter/ml/query/CORSFilter.groovy
@@ -16,8 +16,10 @@ class CORSFilter implements Filter {
     void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse res = servletResponse as HttpServletResponse
         res.addHeader("Access-Control-Allow-Origin", "*")
+        res.addHeader("Access-Control-Allow-Methods", "GET,POST")
         res.addHeader("Access-Control-Allow-Credentials", "true")
         res.addHeader("Access-Control-Allow-Headers", "X-Requested-With")
+        res.addHeader("Access-Control-Max-Age", "1800")
 
         filterChain.doFilter(servletRequest, servletResponse)
     }
