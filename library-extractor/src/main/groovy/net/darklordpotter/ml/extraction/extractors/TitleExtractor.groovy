65a3025166ef0190244faffccce2eb2f0adda5c9
Michael Rose <elementation@gmail.com>
2013-04-02 02:17:13 -0600

    Fixed titles picked up as URLs

--- library-extractor/src/main/groovy/net/darklordpotter/ml/extraction/extractors/TitleExtractor.groovy
+++ library-extractor/src/main/groovy/net/darklordpotter/ml/extraction/extractors/TitleExtractor.groovy
@@ -22,7 +22,14 @@ class TitleExtractor implements DataExtractor {
 
         if (!context.result.title) {
             context.result.title =
-                context.sourceSet.title.replace('&#8216;', '').replace(' - [a-zA-Z0-9]{1,4}', '').replaceAll("â€™", "'")
+                context.sourceSet.title.replace('&#8216;', '')
+                            .replace(' - [a-zA-Z0-9]{1,4}', '')
+                            .replaceAll("â€™", "'")
+                            .replaceAll("&quot;", "")
+        }
+
+        if (context.result.title.contains("http://")) {
+            context.result.title = context.result.title.split("/").last()?.replaceAll("_", " ")
         }
 
 
